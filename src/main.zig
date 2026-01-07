337bd3952f44060d0cbf5b589530f42338dd33bf
Luka <lukabrkovic@proton.me>
2026-01-07 19:47:45 +0100

    feat: examples

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,36 @@
 const std = @import("std");
+const greeter = @import("greeter.zig");
 
 pub fn main() !void {
-    var buffer: [100]u8 = undefined;
-    var stdout = std.fs.File.stdout().writer(&buffer);
+    var stdout_buffer: [4096]u8 = undefined; // 4kb
+    var stdin_buffer: [1024]u8 = undefined; // 1kb
+
+    var stdout = std.fs.File.stdout().writer(&stdout_buffer);
+    var stdin = std.fs.File.stdin().reader(&stdin_buffer);
+
     const writer = &stdout.interface;
-    _ = try writer.write("hello, world!\n");
+    var reader = &stdin.interface;
+
+    _ = try writer.write(greeter.greet());
+    _ = try writer.write("\n");
     try writer.flush();
+
+    while (true) {
+        _ = try writer.write(">zql ");
+        try writer.flush(); // Flush the prompt to the output and clear the buffer
+
+        const line = try reader.takeDelimiter('\n') orelse break;
+        // enter = \r\n = if not that we would get exit\r
+        const trimmed = std.mem.trimRight(u8, line, "\r");
+
+        if (std.mem.eql(u8, trimmed, ".exit")) {
+            _ = try writer.write("Goodbye!\n");
+            try writer.flush();
+            break;
+        }
+
+        _ = try writer.write(trimmed);
+        _ = try writer.write("\n");
+        try writer.flush();
+    }
 }
